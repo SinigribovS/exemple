288edfe74cebe5f8849b971608a1bb619b05c1ce
SinigribovS <71001180+SinigribovS@users.noreply.github.com>
2021-01-26 09:31:02 +0200

    Add files via upload

---
+++ felierede liste.py
@@ -0,0 +1,10 @@
+orase= ['Balti','Chisinau','Cahul','Glodeni','Falesti','Drochia']
+print(orase)
+lista1=orase[0:2]
+lista2=orase[3:6]
+print (lista1)
+print (lista2)
+lista3=orase[:]
+print (lista3)
+lista4=orase[-3:]
+print (lista4)
